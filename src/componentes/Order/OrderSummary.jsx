66bc90940c72bc32060fd88fde1a5354234f39e4
Yennyfer Jarava <yennyfer.jarava92@gmail.com>
2023-09-29 16:13:46 -0500

    Funcionalidad para componente OrderSummary con estilos

--- src/componentes/Order/OrderSummary.jsx
+++ src/componentes/Order/OrderSummary.jsx
@@ -1,25 +1,56 @@
 import React from 'react';
 import style from './OrderSummary.module.css';
+import Swal from 'sweetalert2';
+import lessIcon from './images/less.png';
+import plusIcon from "./images/plus.png";
 
-function OrderSummary({ order, totalOrder, removeProductFromOrder, clearOrder, sendOrder, customer, table }) {
+function OrderSummary({ order, totalOrder, addProductToOrder, removeProductFromOrder, clearOrder, sendOrder, customer, table }) {
+  const handleSendOrder = () => {
+    if (!customer || !table) {
+      Swal.fire('Espera', 'Los campos de cliente y mesa no pueden estar vacíos. Agregue la información antes de ordenar.', 'warning');
+    } else {
+      sendOrder();
+    }
+  };
+  // Calcula el valor total de la orden sumando el precio de cada producto multiplicado por su cantidad
+  const calculateTotal = () => {
+    let total = 0;
+    for (const product of order) {
+      total += product.price * product.quantity;
+    }
+    return total;
+  };
+
   return (
     <div className={style.order}>
-      {customer && <h2>{`👋 Hola  ${customer}`}</h2>}
-      {table && <p>{`Mesa #: ${table}`}</p>}
+      {customer && <h2 className={style.nameCustomer}>{`👋 Hola  ${customer}`}</h2>}
+      {table && <p className={style.numberTable}>{`Mesa # ${table}`}</p>}
+      <h1 className={style.orden}>ORDEN</h1>
+      <hr />
       <div className="order-list">
-        {order.map((product) => (
-          <div key={product.id} className="order-item">
-            <p>{product.name}</p>
-            <p>Quantity: {product.quantity}</p>
-            <p>Precio: ${product.price}</p>
-            <button onClick={() => removeProductFromOrder(product)}>-</button>
-            <button onClick={() => addProductToOrder(product)}>+</button>
-          </div>
-        ))}
+        <table className={style.orderTable}>
+          <tbody>
+            {order.map((product, index) => (
+              <tr key={index}>
+                <td>{product.name}</td>
+                <td>${product.price}</td>
+                  <td><img src={lessIcon} alt="Restar" className={style.iconLess} onClick={() => removeProductFromOrder(product)} />
+                  </td>
+                  <td>{product.quantity}</td>
+                  <td>
+                    <img src={plusIcon} alt="Sumar" className={style.iconPlus} onClick={() => addProductToOrder(product)} />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
-      <p>Total: ${totalOrder}</p>
-      <button onClick={clearOrder}>Limpiar Orden</button>
-      <button onClick={sendOrder}>Enviar a Cocina</button>
+      <p className={style.totalOrder}>TOTAL ${calculateTotal()}</p>
+      <hr />
+      <div className={style.btns}>
+        <button className={style.btnClear} onClick={clearOrder}>LIMPIAR</button>
+        <button className={style.btnSend} onClick={handleSendOrder} >ORDENAR</button>
+      </div>
     </div>
   );
 }

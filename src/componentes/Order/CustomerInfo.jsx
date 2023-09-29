a30d944c01dc1a8c1b6fe63afdcdd64257df2495
Yennyfer Jarava <yennyfer.jarava92@gmail.com>
2023-09-29 16:11:21 -0500

    Creación de handleCustomerChange y Table

--- src/componentes/Order/CustomerInfo.jsx
+++ src/componentes/Order/CustomerInfo.jsx
@@ -2,20 +2,29 @@ import React from 'react';
 import style from './CustomerInfo.module.css';
 
 function CustomerInfo({ customer, table, setCustomer, setTable }) {
+
+  const handleCustomerChange = (e) => {
+    setCustomer(e.target.value);
+  };
+
+  const handleTableChange = (e) => {
+    setTable(e.target.value);
+  };
+
   return (
     <div className="customer-table">
       <input
         type="text"
         placeholder="Nombre del cliente"
         value={customer}
-        onChange={(e) => setCustomer(e.target.value)}
+        onChange={handleCustomerChange}
         className={style.inputCustomerName}
       />
       <input
-        type="text"
-        placeholder="Número de mesa"
+        type="number"
+        placeholder="No. de mesa"
         value={table}
-        onChange={(e) => setTable(e.target.value)}
+        onChange={handleTableChange}
         className={style.inputTableNumber}
       />
     </div>

fff2a91168c99a2ea73e009ed1a47a4b23887a7f
LeslieGaribay <131633753+LeslieGaribay@users.noreply.github.com>
2023-11-06 16:09:18 -0600

    Estructura inicial Botones y navegación, OrderList

--- src/componentes/OrderList/OrderList.jsx
+++ src/componentes/OrderList/OrderList.jsx
@@ -1,13 +1,25 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate, Link } from "react-router-dom";
 import style from "./OrderList.module.css";
 import OrderCard from './OrderCard';
-import { Link } from "react-router-dom";
 import update from "./images/update.png";
 import { getOrdersFromBackend } from "../../services/orders-services";
+
+import logout from "./images/flecha-logout.png";
+import logo from "./images/logo_bq.png";
 
 export function OrderList() {
   const [orderList, setOrderList] = useState([]);
   const [activeTab, setActiveTab] = useState(1);
+
+  const navigate = useNavigate();
+  const handleLogoutClick = () => {
+    navigate("/login");
+  };
+
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
 
   useEffect(() => {
     refreshOrderList();
@@ -22,31 +34,50 @@ export function OrderList() {
     }
   };
 
-  const handleTabClick = (tab) => {
-    setActiveTab(tab);
-  };
-
   return (
     <div className={style.orderList}>
-      <div className={style.tabs}>
-        <Link
-          to="/order"
-          className={activeTab === 1 ? `${style.tabOrder} active` : style.tabOrder}
-          onClick={() => handleTabClick(1)}
-        >
-          Ordenar
-        </Link>
-        <Link
-          to="/order-list"
-          className={activeTab === 2 ? `${style.tabOrderList} active` : style.tabOrderList}
-          onClick={() => handleTabClick(2)}
-        >
-          Lista de Órdenes
-        </Link>
+      <div className={style.navbarKitchen}>
+        <nav>
+          <div className="navbar-left">
+            <img
+              src={logout}
+              alt="logout"
+              className="navbar-image-logout"
+            />
+            <p className="navbar-logout" onClick={handleLogoutClick}>
+              Salir
+            </p>
+          </div>
+          <div className="navbar-right">
+            <img
+              src={logo}
+              alt="Imagen 2"
+              className="navbar-image-logo"
+            />
+          </div>
+        </nav>
+      </div>
+      <div className={style.orderList}>
+        <div className={style.tabs}>
+          <Link
+            to="/order"
+            className={activeTab === 1 ? `${style.tabOrder} active` : style.tabOrder}
+            onClick={() => handleTabClick(1)}
+          >
+            Ordenar
+          </Link>
+          <Link
+            to="/order-list"
+            className={activeTab === 2 ? `${style.tabOrderList} active` : style.tabOrderList}
+            onClick={() => handleTabClick(2)}
+          >
+            Lista de Órdenes
+          </Link>
+        </div>
       </div>
       <div className={style.orderListTitle}>Órdenes</div>
       <h2>Hola, Mesero Juan</h2>
-      <img src={update} alt="updateOrders" className={style.updateIcon} onClick={() => refreshOrderList()}/>
+      <img src={update} alt="updateOrders" className={style.updateIcon} />
       <div className={style.orderCardsSection}>
         {!orderList ? (
           <div className={style.loadingSpinner}></div>

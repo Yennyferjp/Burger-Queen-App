ee93edc64a0f244e360efaa67f40966341337ac7
Yennyferjp <yennyfer.jarava92@gmail.com>
2023-11-07 11:00:04 -0500

    Cambios para Navegar a Order cuando el User es Waiter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import { Login } from './componentes/Login/login';
 import { Order } from './componentes/Order/order';
@@ -10,36 +11,30 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [user, setUser] = useState([]);
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+
+  const handleLogin = (loggedInUser) => {
+    setUser(loggedInUser);
+
+    if (loggedInUser.rol === "waiter") {
+      navigate("/order");
+    }else if (loggedInUser.rol == "admin"){
+       navigate("/users");
+    }
+  }
 
   return (
-    <Router>
       <div className="App">
         <Routes>
-          {/* Ruta para la página de inicio de sesión */}
-          <Route path="/login" element={<Login setUser={setUser} />} />
-
-          {/* Ruta para la página de pedidos */}
-          <Route path="/order" element={<Order />}
-          />
-
-          {/* Ruta para la página de usuarios */}
-          <Route path="/users" element={<Users />}
-          />
-
-          {/* Ruta para la página de productos */}
-          <Route path="/products" element={<Products />}
-          />
-
-          {/* Ruta para la página de listado de órdenes */}
-          <Route path="/order-list" element={<OrderList />}
-          /> 
-
-          {/* Redirección predeterminada a la página de inicio de sesión */}
-          <Route path="*" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<Login setUser={handleLogin} />} />
+          <Route path="/order" element={<Order />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/order-list" element={<OrderList />} />
+          <Route path="/" element={<Navigate to="/login" />} />
         </Routes>
       </div>
-    </Router>
   );
 }
 

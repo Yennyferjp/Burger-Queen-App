eef21587731543e1b677e322888c3c8db65b8c42
Yennyferjp <yennyfer.jarava92@gmail.com>
2023-12-07 13:32:05 -0500

    Mejoras en el orden y estilo de para el mesero

--- src/componentes/OrderList/OrderCard.jsx
+++ src/componentes/OrderList/OrderCard.jsx
@@ -13,25 +13,26 @@ function OrderCard({ order, onCheckClicked }) {
   return (
     <div className={style.orderCard}>
       <div className={style.cardHeader}>
-        <h4>{`Mesa # ${order.table}`}</h4>
+        <h4 className={style.h4Mesa}>{`Mesa # ${order.table}`}</h4>
+        
+      </div>
+      <hr style={{ borderColor: '#F7DC34' }} />
+      <div className={style.orderSection}>
+        {order.products.map((producto, index) => (
+          <div className={style.productQuantity} key={index}>
+            <h2 className={style.productName}> ✧ {producto.product.name}</h2>
+            <h2 className={style.productqty}> {producto.qty}</h2>
+          </div>
+        ))}
+      </div>
+      <div className={style.orderStatusMessage}>
+        <p> {order.status}</p>
         <img
           src={checkIcon}
           alt="check"
           className={style.checkIcon}
           onClick={handleCheckClick}
         />
-      </div>
-      <hr></hr>
-      <div className={style.orderSection}>
-        {order.products.map((producto, index) => (
-          <div className={style.productQuantity} key={index}>
-            <h2>{producto.product.name}</h2>
-            <h2>{producto.qty}</h2>
-          </div>
-        ))}
-      </div>
-      <div className={style.orderStatusMessage}>
-        <p>Estado de la orden: {order.status}</p>
       </div>
     </div>
   );

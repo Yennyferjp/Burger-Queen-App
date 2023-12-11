35f7b00dc3ac3f07bb5d64d1b7c8614e1aaae757
Yennyferjp <yennyfer.jarava92@gmail.com>
2023-12-11 09:02:29 -0500

    Cambio de Icon y mejoría en diseño en OrderCard

--- src/componentes/OrderList/OrderCard.jsx
+++ src/componentes/OrderList/OrderCard.jsx
@@ -1,20 +1,21 @@
 import Swal from 'sweetalert2';
 import style from "./OrderCard.module.css";
 import check from "./images/check.png";
-import pendingIcon from "./images/pendingIcon.png"; 
+import cancelIcon from "./images/cancelIcon.png"; 
 
 function OrderCard({ order, onCheckClicked }) {
   const handleCheckClick = async () => {
     onCheckClicked(order._id, order.status);
   };
 
-  const checkIcon = order.status === 'PENDIENTE' ? pendingIcon : check;
+  const checkIcon = order.status === 'PENDIENTE' ? cancelIcon : check;
+  const iconCancel = order.status === 'PENDIENTE' ? style.cancelIcon : '';
 
   return (
     <div className={style.orderCard}>
       <div className={style.cardHeader}>
         <h4 className={style.h4Mesa}>{`Mesa # ${order.table}`}</h4>
-        
+        <p className={style.orderStatusMessage}> {order.status}</p>
       </div>
       <hr style={{ borderColor: '#F7DC34' }} />
       <div className={style.orderSection}>
@@ -25,12 +26,11 @@ function OrderCard({ order, onCheckClicked }) {
           </div>
         ))}
       </div>
-      <div className={style.orderStatusMessage}>
-        <p> {order.status}</p>
+      <div >
         <img
           src={checkIcon}
           alt="check"
-          className={style.checkIcon}
+          className={`${style.checkIcon} ${iconCancel}`}
           onClick={handleCheckClick}
         />
       </div>
